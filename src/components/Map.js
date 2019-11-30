4b7dc8777695bc69ee37b1a66138e371a12226c0
Morshed <morshedalamustc22@gmail.com>
2019-11-30 13:36:00 +0600

    Polyline draw on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import {Text, StyleSheet} from "react-native";
 import MapView from 'react-native-maps';
+import MapPolyline from "react-native-maps/lib/components/MapPolyline";
 const Map = ()=>{
-    return <Text>I am a map</Text>;
+    let points = [];
+    for (let i =0; i <20; i++ ){
+        points.push({
+            latitude:23.8103 + i * 0.001,
+            longitude:90.4125 + i * 0.001
+        });
+    }
+    return(
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                    latitude:23.8103,
+                    longitude:90.4125,
+                    latitudeDelta:0.01,
+                    longitudeDelta:0.01
+                }}>
+            <MapPolyline coordinates={points}/>
+        </MapView>
+    );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map:{
+        height:300
+    }
+});
 export default Map;

47ad4f97f8bfe28024bfe18817879d55ff3e5a91
Morshed <morshedalamustc22@gmail.com>
2019-12-01 01:37:21 +0600

    Location tracker added and used reducer to manipulate

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import {Text, StyleSheet} from "react-native";
-import MapView from 'react-native-maps';
-import MapPolyline from "react-native-maps/lib/components/MapPolyline";
-const Map = ()=>{
-    let points = [];
-    for (let i =0; i <20; i++ ){
-        points.push({
-            latitude:23.8103 + i * 0.001,
-            longitude:90.4125 + i * 0.001
-        });
+import React, {useContext} from 'react';
+import {Text, StyleSheet, ActivityIndicator} from "react-native";
+import MapView, {Polyline} from 'react-native-maps';
+import {Context as LocationContext} from '../context/LocationContext'
+
+const Map = () => {
+    const {state: {currentLocation}} = useContext(LocationContext);
+    if (!currentLocation) {
+        return <ActivityIndicator size="large" style={{marginTop: 200}}/>
     }
-    return(
+    return (
         <MapView
             style={styles.map}
             initialRegion={{
-                    latitude:23.8103,
-                    longitude:90.4125,
-                    latitudeDelta:0.01,
-                    longitudeDelta:0.01
-                }}>
-            <MapPolyline coordinates={points}/>
-        </MapView>
+                ...currentLocation.coords,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            }}
+            region={{
+                ...currentLocation.coords,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            }}
+        />
     );
 };
 const styles = StyleSheet.create({
-    map:{
-        height:300
+    map: {
+        height: 300
     }
 });
 export default Map;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text} from "react-native";
 import {SafeAreaView} from 'react-navigation'
 import {requestPermissionsAsync, watchPositionAsync, Accuracy} from "expo-location";
 import Map from "../components/Map";
+import {Context as LocationContext } from '../context/LocationContext'
 
 const TrackCreateScreen = ()=>{
+    const {addLocation} = useContext(LocationContext);
     const [err, setErr] = useState(null);
     const startWatching = async ()=>{
         try{
@@ -13,9 +15,9 @@ const TrackCreateScreen = ()=>{
             await watchPositionAsync({
                 accuracy:Accuracy.BestForNavigation,
                 timeInterval:1000,
-                distanceInterval:10
+                distanceInterval:0.1
             },(location)=>{
-               console.log(location);
+               addLocation(location)
             } );
         }catch (e) {
             setErr(e);
@@ -27,7 +29,7 @@ const TrackCreateScreen = ()=>{
 
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
-            <Text>Create a Track</Text>
+            <Text style={{fontSize:48}}>Create a Track</Text>
             <Map/>
             {err? <Text>Please enable location services</Text>:null}
         </SafeAreaView>

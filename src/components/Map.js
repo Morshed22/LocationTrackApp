cbf7cce558da5a48de935e3dc0b9d0df4df172c2
Morshed <morshedalamustc22@gmail.com>
2019-11-30 13:11:35 +0600

    Map installed

---
+++ src/components/Map.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text, StyleSheet} from "react-native";
+import MapView from 'react-native-maps';
+const Map = ()=>{
+    return <Text>I am a map</Text>;
+};
+const styles = StyleSheet.create({});
+export default Map;
